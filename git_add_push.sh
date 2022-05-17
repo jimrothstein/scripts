5eb9dbd2f5a7934c36802d2165793eb2c8e8c062
jimrothstein <jimrothstein@gmail.com>
2022-05-17 01:31:06 -0700

    wip

--- git_add_push.sh
+++ git_add_push.sh
@@ -7,12 +7,18 @@ add_commit_push()
 	#		Add files, Commit, Push
 	line="==========================="
 	echo $(date) 
+	echo "status ..." $line
 	git s
 	echo $line
 	git a
+
+	echo $line
 	git cm wip
+	echo $line
 	git push
+	echo $line
 	git s
+	echo $line
 }
 
 add_commit_push
