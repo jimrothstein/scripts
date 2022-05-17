4f9036b1a6a06fc3fcd463a08d466a3912f252ec
jimrothstein <jimrothstein@gmail.com>
2022-05-17 01:40:40 -0700

    wip

--- git_add_push.sh
+++ git_add_push.sh
@@ -6,25 +6,28 @@ add_commit_push()
 {
 	#		Add files, Commit, Push
 	line="==========================="
+	echo "\n\n"
+	echo "BEGIN ..." $line
 	echo $(date) 
 
-	echo " "
+	echo "\n\n"
 	echo "STATUS ..." $line
 	git s
 
-	echo " "
+	
+	echo "\n\n"
 	echo "ADD FILES ..." $line
 	git a
 
-	echo " "
+	echo "\n\n"
 	echo "COMMIT ..." $line
 	git cm wip
 	
-	echo " "
+	echo "\n\n"
 	echo "PUSH ..." $line
 	git push
 	
-	echo " "
+	echo "\n\n"
 	echo "FINAL STATUS ..." $line
 	git s
 	echo $line
